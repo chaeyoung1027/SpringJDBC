3a568c4ff69028535def01feb0f28d33f4b36417
Chaeyoung Lim <rnldyal1027@naver.com>
2023-04-12 12:19:53 +0900

    basic setting

--- src/main/java/kr/hs/study/config/BeanConfig.java
+++ src/main/java/kr/hs/study/config/BeanConfig.java
@@ -1,2 +1,35 @@
-package kr.hs.study.config;public class BeanConfig {
+package kr.hs.study.config;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class BeanConfig {
+
+    @Configuration
+    @ComponentScan(basePackages = {"kr.hs.study.beans","kr.hs.study.db"})
+    public class BeanConfigClass {
+        //Datasource
+        @Bean
+        public BasicDataSource source() {
+            BasicDataSource source = new BasicDataSource();
+            source.setDriverClassName("oracle.jdbc.OracleDriver");
+            source.setUrl("jdbc:oracle:thin:@localhost:1521:xe");
+            source.setUsername("scott");
+            source.setPassword("tiger");
+
+            return source;
+        }
+
+        // 데이터베이스에 접속해서 쿼리를 전달하는 빈을 등록한다.
+        //실제 데이터베이스에 접속해서 쿼리를 날려주는 애 : JDBCTemplate 이다.
+        @Bean
+        public JdbcTemplate db(BasicDataSource source) {
+            JdbcTemplate db = new JdbcTemplate(source);
+            return db;
+        }
+    }
+
 }
